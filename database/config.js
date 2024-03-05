34da423b6d927d604db9d0a30d6e9c9555294dbe
ktsuny <ktsuny@berkeley.edu>
2024-03-05 14:19:10 -0800

    create is working

--- database/config.js
+++ database/config.js
@@ -6,10 +6,17 @@ require("dotenv").config();
 
 // Create a 'connection pool' using the provided credentials
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_DATABASE,
+  connectionLimit: 10,
+  waitForConnections: true,
+  host: 'classmysql.engr.oregonstate.edu',
+  user: 'cs340_',
+  password: '',
+  database: 'cs340_',
+  port: 3306,
 }).promise();
 
 
